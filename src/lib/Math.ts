4e90aa3650a7dd9587cd9262b8991fa07dab6152
Jerry Lum <serverofjerry@gmail.com>
2025-02-23 02:35:56 +0800

    Remove deadband function

--- src/lib/Math.ts
+++ src/lib/Math.ts
@@ -8,7 +8,3 @@
 export function clamp(value: number, min: number, max: number) {
 	return Math.min(Math.max(value, min), max);
 }
-
-export function deadband(value: number, deadband: number) {
-	return Math.abs(value) < deadband ? 0 : value;
-}
